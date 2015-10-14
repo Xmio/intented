31fa7d7aab9ecd4dbeac3a2d22f76a4c68eeef00
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-10-13 23:26:09 -0300

    Adds the Integration Test framework
    We are currently not doing any integration tests, so, this commit adds
    the framework (with the appropriate changes) and an example and very
    simple test to validate it works.
    
    IT framework: http://github.com/caarlos0/it

--- server/datastores/database.go
+++ server/datastores/database.go
@@ -1,14 +1,13 @@
 package datastores
 
 import (
+	"database/sql"
 	"log"
-
-	"github.com/jmoiron/sqlx"
 )
 
-// NewDBConnectionPool creates a new sqlx.DB connecting to DATABASE_URL
-func NewDBConnectionPool(url string) *sqlx.DB {
-	db, err := sqlx.Open("postgres", url)
+// NewDBConnectionPool creates a new sql.DB connecting to DATABASE_URL
+func NewDBConnectionPool(url string) *sql.DB {
+	db, err := sql.Open("postgres", url)
 	if err != nil {
 		log.Fatal(err)
 	}
